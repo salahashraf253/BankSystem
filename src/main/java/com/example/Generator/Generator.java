3ccd65e2a32f84cc768951d49c817a19925c31af
salahashraf253 <salahashraf924@gmail.com>
2022-04-18 22:27:14 +0200

    Make Password generator class

---
+++ src/main/java/com/example/Generator/Generator.java
@@ -0,0 +1,5 @@
+package com.example.Generator;
+
+public interface Generator {
+    String generate();
+}
---
+++ src/main/java/com/example/Generator/PassWordGenerator.java
@@ -0,0 +1,15 @@
+package com.example.Generator;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PassWordGenerator implements Generator{
+
+    @Override
+    public String generate() {
+
+        return null;
+    }
+
+}
+
+
