59fb4034996d949a4a3f3b73a94dda3ebf524040
salahashraf253 <salahashraf924@gmail.com>
2022-04-19 19:25:25 +0200

    add comment

--- src/main/java/com/example/Generator/Generator.java
+++ src/main/java/com/example/Generator/Generator.java
@@ -1,5 +1,10 @@
 package com.example.Generator;
 
+/*
+we have to generate two Strings
+ temp password for forget password option
+authentication  code for sign up option
+* */
 public interface Generator {
     String generate();
 }
