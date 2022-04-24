1e22acb0a671a78244a486a9a5c765e403222cb4
Salah <salahashraf924@gmail.com>
2022-04-24 18:02:43 +0200

    update

--- src/main/java/com/example/banksystem/Loan/LoanFactory.java
+++ src/main/java/com/example/banksystem/Loan/LoanFactory.java
@@ -5,13 +5,13 @@ import java.util.Objects;
 
 public class LoanFactory {
     public static Loan getLoan(String loanType) {
-        if(loanType.compareTo("Educational")==0){ //educational
+        if(loanType.compareTo("Educational")==0 ||loanType.compareTo("e")==0){ //educational
             return new EducationalLoan();
         }
-        else if(loanType.compareTo("Home")==0){   //home
+        else if(loanType.compareTo("Home")==0 ||loanType.compareTo("h")==0){   //home
             return new HomeLoan();
         }
-        else if(loanType.compareTo("Personal")==0){    //personal
+        else if(loanType.compareTo("Personal")==0||loanType.compareTo("p")==0){    //personal
             return new PersonalLoan();
         }
         return null;
