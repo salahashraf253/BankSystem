d0c66e6563396584c1e2fa05f5d796004973e610
Salah <salahashraf924@gmail.com>
2022-04-29 20:37:38 +0200

    update NotifierEmail.java

--- src/main/java/com/example/notification/NotifierEmail.java
+++ src/main/java/com/example/notification/NotifierEmail.java
@@ -3,7 +3,8 @@ package com.example.notification;
 public class NotifierEmail extends Email{
 
     public void sendNotifierEmail(String recipient, String subject, String body){
+        super.setEmailSubject(subject);
+        super.setEmailBody(body);
         super.sendEmail(recipient);
-
     }
 }
