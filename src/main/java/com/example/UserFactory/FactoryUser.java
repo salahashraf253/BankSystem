457945f89552b869fd08d0354bda7b9d3b05a8b4
Salah <salahashraf924@gmail.com>
2022-04-30 14:30:38 +0200

    make the function static
    to decrease the new keyword in the application

--- src/main/java/com/example/UserFactory/FactoryUser.java
+++ src/main/java/com/example/UserFactory/FactoryUser.java
@@ -2,9 +2,8 @@ package com.example.UserFactory;
 
 public class FactoryUser {
 
-    public User getUserType (String userType) {
+    public static User getUserType (String userType) {
         // this is supposed to be in a form of combobox-ish
-
         if (userType.equalsIgnoreCase("s")) {   //s for staff
             return new Staff();
         } else if (userType.equalsIgnoreCase("c")) { //c for client
