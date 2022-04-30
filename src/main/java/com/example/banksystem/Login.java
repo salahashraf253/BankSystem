97cac2e000fe047ca411ca13023092109b5b8c88
Salah <salahashraf924@gmail.com>
2022-04-30 17:30:28 +0200

    update the code to be more clean

--- src/main/java/com/example/banksystem/Login.java
+++ src/main/java/com/example/banksystem/Login.java
@@ -12,25 +12,13 @@ import java.sql.SQLException;
 
 public class Login {
 
-    public User validateLogin(String userID, String password) throws SQLException {
+    public static User validateLogin(String userID, String password) throws SQLException {
         DataBaseReader dataBaseReader= new DataBaseReader();
         String query="select * from user where user_id='" + EncrpytString.getEncrpytedString(userID)
                 + "' and password='" + EncrpytString.getEncrpytedString(password)+"';";
         ResultSet resultSet=dataBaseReader.read(query);
         User user= DataBaseMapping.getUser(resultSet);
-        if(user==null){
-            //Invalid user id or password
-            return null;
-        }
-        System.out.println("Email: "+user.getEmail());
         dataBaseReader.closeConnection();
         return user;
     }
-
-    public static void main(String[]args) throws SQLException {
-        Login login=new Login();
-        User user=login.validateLogin("12","1233123123");
-
-    }
-
 }
