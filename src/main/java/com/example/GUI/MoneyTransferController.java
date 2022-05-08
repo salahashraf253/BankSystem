01cd5ba7409621210b668d15beb58d3de0ab6b50
Ahmed Sobhy <a7mdsobhy@gmail.com>
2022-05-08 21:00:15 +0200

    Money Transfer Tap in AccountLayout

---
+++ src/main/java/com/example/GUI/MoneyTransferController.java
@@ -0,0 +1,34 @@
+package com.example.GUI;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MoneyTransferController implements Initializable {
+    @FXML
+    TextField Amount_txt;
+    @FXML
+    TextField from_txt;
+
+    @FXML
+    TextField to_txt;
+    int toAccount;
+    double Amount;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        // account number of user from database put it in setText
+        from_txt.setText("123");
+    }
+    @FXML
+    public void TransferMoney(){
+        toAccount=Integer.parseInt( to_txt.getText());
+        Amount= Integer.parseInt( Amount_txt.getText());
+        // put this Amount in Account where Account number=toAccount
+
+    }
+}
