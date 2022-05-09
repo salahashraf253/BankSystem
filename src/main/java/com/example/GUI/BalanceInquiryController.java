c71c998b50fed7c904ca0466e0a242f349dbf6b2
Salah <salahashraf924@gmail.com>
2022-05-09 03:17:18 +0200

    update balance inquiry
    connect the labels of account type & balance

--- src/main/java/com/example/GUI/BalanceInquiryController.java
+++ src/main/java/com/example/GUI/BalanceInquiryController.java
@@ -1,11 +1,15 @@
 package com.example.GUI;
 
+import com.example.UserFactory.Client;
+import com.example.UserFactory.User;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TextField;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import static com.example.GUI.LayoutController.user;
 
 public class BalanceInquiryController implements Initializable {
     @FXML
@@ -18,7 +22,15 @@ public class BalanceInquiryController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         // Read from database type and balance of account
-        type_txt.setText("Saving Account");
-        balance_txt.setText("1234");
+        try {
+            User u= (Client) user;
+            type_txt.setText(((Client) u).getAccount().getAccountType());
+            String balance= String.valueOf(((Client) u).getAccount().getBalance());
+            balance_txt.setText(balance);
+        }
+        catch (Exception e){
+            System.out.println("Error in Balance Inquiry");
+            System.out.println(e.getMessage());
+        }
     }
 }
