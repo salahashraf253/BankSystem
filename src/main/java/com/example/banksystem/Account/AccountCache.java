31e80cbae46d5ddf97dfd4fb1e17265278e741ec
Salah <salahashraf924@gmail.com>
2022-05-09 03:32:58 +0200

    update loadCache method

--- src/main/java/com/example/banksystem/Account/AccountCache.java
+++ src/main/java/com/example/banksystem/Account/AccountCache.java
@@ -13,13 +13,35 @@ public class AccountCache {
         return (Account) cachedAccount.clone();
     }
 
-    public static void loadCache() {
-        
-        SavingsAccount savings = new SavingsAccount();
-        savings.setAccount_no(1);
-        String savings_id = Integer.toString(savings.getAccount_no());
-        accountMap.put(savings_id,savings);
-        
+    public static void loadCache(){
+        if(accountMap.size()==0){
+            try {
+                System.out.println("Loading Account Map");
+                SavingsAccount savings = new SavingsAccount();
+                savings.setAccount_no(1);
+                String savings_id = Integer.toString(savings.getAccount_no());
+                accountMap.put(savings_id,savings);
+                Account mm = new MoneyMarket();
+                savings.setAccount_no(2);
+                String mm_id = Integer.toString(mm.getAccount_no());
+                accountMap.put(mm_id,mm);
+
+                Account check_acc = new CheckingAccount();
+                savings.setAccount_no(3);
+                String check_id = Integer.toString(check_acc.getAccount_no());
+                accountMap.put(check_id,check_acc);
+
+                Account cer = new CertificateDeposit();
+                cer.setAccount_no(4);
+                String cer_id = Integer.toString(cer.getAccount_no());
+                accountMap.put(cer_id,cer);
+            }
+            catch (Exception e){
+                System.out.println("Error in load cash ");
+                System.out.println(e.getMessage());
+            }
+        }
+        else System.out.println("Account Map is already loaded");
     }
         
 }
