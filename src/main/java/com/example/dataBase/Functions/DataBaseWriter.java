7004b65ad781f2d7d2c106b2c443a7688a90aaa6
Salah <salahashraf924@gmail.com>
2022-05-10 20:53:56 +0200

    update

--- src/main/java/com/example/dataBase/Functions/DataBaseWriter.java
+++ src/main/java/com/example/dataBase/Functions/DataBaseWriter.java
@@ -1,9 +1,5 @@
 package com.example.dataBase.Functions;
 
-import com.example.Decorator.EncryptStringProvider;
-import com.example.Decorator.PlainStringProvider;
-import com.example.Decorator.StringProvider;
-import com.example.Decorator.StringProviderDecorator;
 import com.example.dataBase.DataBase;
 
 import java.sql.Connection;
@@ -28,54 +24,65 @@ public class DataBaseWriter extends DataBase {
 
     //Salah: test the database writer with encryption
     public static void main(String []args) throws SQLException {
-        String user_id="100100";
-        user_id=EncrpytString.getEncrpytedString(user_id);
-        DataBaseWriter dataBaseWriter=new DataBaseWriter();
-        String firstName="Salah";
-        firstName=EncrpytString.getEncrpytedString(firstName);
-
-        System.out.println("Name :"+firstName);
-        String ssn="30110211401634";
-        ssn=EncrpytString.getEncrpytedString(ssn);
-        String email="salahashraf924@gmail.com";
-        email=EncrpytString.getEncrpytedString(email);
-        String password="123456Ab#";
-        password=EncrpytString.getEncrpytedString(password);
-
-        String lastName="Ashraf";
-        lastName=EncrpytString.getEncrpytedString(lastName);
-        String salary="5000";
-        salary=EncrpytString.getEncrpytedString(salary);
-
-        String phone="01102527521";
-        phone=EncrpytString.getEncrpytedString(phone);
-        Date dob=new Date(2001,10,21);
-        String address="Cairo";
-        address=EncrpytString.getEncrpytedString(address);
-        String status="s";
-        status=EncrpytString.getEncrpytedString(status);
-        String gender="m";
-        gender=EncrpytString.getEncrpytedString(gender);
-        String type="c";
-        type=EncrpytString.getEncrpytedString(type);
-        String query="insert into user" +
-                " (user_id,ssn,email,password,firstName,lastName, " +
-                "salary, phone,dob,address,status,gender,type)"+
-                "VALUES(" +
-                "'" + user_id + "'," +
-                        "'" + ssn + "'," +
-                        "'" + email + "'," +
-                        "'" + password + "'," +
-                        "'" + firstName + "'," +
-                        "'" + lastName + "'," +
-                        "'" + salary + "'," +
-                        "'"+phone+"',"+
-                        "'"+dob+"',"+
-                        "'"+address+"',"+
-                        "'"+status+"',"+
-                         "'"+gender+"',"+
-                          "'"+type+"'"+
-                              ")";
-        dataBaseWriter.write(query);
+//        String user_id="100100";
+//        user_id=EncrpytString.getEncrpytedString(user_id);
+//        DataBaseWriter dataBaseWriter=new DataBaseWriter();
+//        String firstName="Salah";
+//        firstName=EncrpytString.getEncrpytedString(firstName);
+//
+//        System.out.println("Name :"+firstName);
+//        String ssn="30110211401634";
+//        ssn=EncrpytString.getEncrpytedString(ssn);
+//        String email="salahashraf924@gmail.com";
+//        email=EncrpytString.getEncrpytedString(email);
+//        String password="123456Ab#";
+//        password=EncrpytString.getEncrpytedString(password);
+//
+//        String lastName="Ashraf";
+//        lastName=EncrpytString.getEncrpytedString(lastName);
+//        String salary="5000";
+//        salary=EncrpytString.getEncrpytedString(salary);
+//
+//        String phone="01102527521";
+//        phone=EncrpytString.getEncrpytedString(phone);
+//        Date dob=new Date(2001,10,21);
+//        String address="Cairo";
+//        address=EncrpytString.getEncrpytedString(address);
+//        String status="s";
+//        status=EncrpytString.getEncrpytedString(status);
+//        String gender="m";
+//        gender=EncrpytString.getEncrpytedString(gender);
+//        String type="checking account";
+//        type=EncrpytString.getEncrpytedString(type);
+////        String query="insert into user" +
+////                " (user_id,ssn,email,password,firstName,lastName, " +
+////                "salary, phone,dob,address,status,gender,type)"+
+////                "VALUES(" +
+////                "'" + user_id + "'," +
+////                        "'" + ssn + "'," +
+////                        "'" + email + "'," +
+////                        "'" + password + "'," +
+////                        "'" + firstName + "'," +
+////                        "'" + lastName + "'," +
+////                        "'" + salary + "'," +
+////                        "'"+phone+"',"+
+////                        "'"+dob+"',"+
+////                        "'"+address+"',"+
+////                        "'"+status+"',"+
+////                         "'"+gender+"',"+
+////                          "'"+type+"'"+
+////                              ")";
+//
+////        String account_id=EncrpytString.getEncrpytedString("1");;
+////        String balance=EncrpytString.getEncrpytedString("50000");;
+//        String q="insert into bank_account" +
+//                " (user_id,account_id,balance,type)"+
+//                "VALUES(" +
+//                "'" + user_id + "'," +
+//                "'" + account_id + "'," +
+//                "'" + balance + "'," +
+//                "'"+type+"'"+
+//                ")";
+//        dataBaseWriter.write(q);
     }
 }
