1fa151d4c0659ba60161d7b2263cf7cffd50057b
Salah <salahashraf924@gmail.com>
2022-05-10 20:47:32 +0200

    update

--- src/main/java/com/example/GUI/AccountHistoryController.java
+++ src/main/java/com/example/GUI/AccountHistoryController.java
@@ -1,6 +1,7 @@
 package com.example.GUI;
 
 import com.example.banksystem.Account.Account;
+import com.example.banksystem.Transaction;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
@@ -10,34 +11,51 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 
 import java.net.URL;
+import java.sql.Date;
 import java.util.ResourceBundle;
 
 public class AccountHistoryController implements Initializable {
     @FXML
-    private TableColumn amount_col;
-
+    private TableColumn<Transaction, Float> amount_col;
     @FXML
-    private TableColumn date_col;
-
+    private TableColumn<Transaction, Date> date_col;
     @FXML
     private TableView history_table;
-
     @FXML
-    private TableColumn status_col;
-
+    private TableColumn<Transaction,String> status_col;
     @FXML
-    private TableColumn trans_col;
-    ObservableList list = FXCollections.observableArrayList();
+    private TableColumn<Transaction, Integer> trans_col;
 
-    public ObservableList getTransaction(){
-        // add transaction table where online account id in this list
-        return list;
-    }
+//    ObservableList list = FXCollections.observableArrayList();
+    ObservableList<Transaction> list = FXCollections.observableArrayList();
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        trans_col.setCellValueFactory(new PropertyValueFactory<>("transaction_id") );
-        amount_col.setCellValueFactory(new PropertyValueFactory<>("amount") );
-        date_col.setCellValueFactory(new PropertyValueFactory<>("date") );
-        status_col.setCellValueFactory(new PropertyValueFactory<>("status") );
+        try{
+            trans_col.setCellValueFactory(new PropertyValueFactory<>("transactionID") );
+            amount_col.setCellValueFactory(new PropertyValueFactory<>("amount") );
+            date_col.setCellValueFactory(new PropertyValueFactory<>("date") );
+            status_col.setCellValueFactory(new PropertyValueFactory<>("type") );
+//            history_table.getColumns().setAll(trans_col,amount_col,date_col,status_col);
+            loadTransactions();
+        }
+        catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+
+    }
+    public void loadTransactions(){
+//        try{
+//            list= Transaction.getTransactions(4123);
+//            System.out.println("Size of list is "+list.size());
+//            history_table.setItems(list);
+//        }
+//        catch (Exception e){
+//            System.out.println("Error in Account history controller");
+//            System.out.println(e.getMessage());
+//        }
+
+        list.add(new Transaction(1312,1000,"1-1-2001","withdraw"));
+        history_table.setItems(list);
     }
 }
