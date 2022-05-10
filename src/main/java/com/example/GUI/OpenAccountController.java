53811970a47aa309ad3106ec52a7ec1de468d0ee
Salah <salahashraf924@gmail.com>
2022-05-11 00:51:29 +0200

    implement open account in the database

--- src/main/java/com/example/GUI/OpenAccountController.java
+++ src/main/java/com/example/GUI/OpenAccountController.java
@@ -1,20 +1,26 @@
 package com.example.GUI;
 
+import com.example.Generator.AccountIDGenerator;
+import com.example.UserFactory.Client;
+import com.example.banksystem.Account.Account;
+import com.example.banksystem.Account.FactoryAccount;
+import com.example.notification.AuthenticationEmail;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.ComboBox;
+import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
 
-import java.io.IOException;
+import javax.crypto.Cipher;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 
 public class OpenAccountController implements Initializable {
 
+    @FXML
+    public DatePicker DateOfBirth;
+    public RadioButton male_Radio_Btn;
+    public RadioButton female_Radio_Btn;
     @FXML
     AnchorPane open_account_pane;
     @FXML
@@ -25,6 +31,8 @@ public class OpenAccountController implements Initializable {
     ComboBox<String> acc_type_cmb;
     @FXML
     CheckBox single_checkbox, married_checkbox, divorced_checkbox, widowed_checkbox;
+    @FXML
+    TextField Id_passport ,address ,name,email ,salary,phoneNum;
 
     PageLoader pageLoader = new PageLoader();
     @Override
@@ -34,7 +42,7 @@ public class OpenAccountController implements Initializable {
                 "Computer Programmer", "Chef", "Dentist", "Doctor", "Electrician", "Engineer", "Factory Worker", "Farmer",
                 "Fireman", "Flight Attendant", "Judge", "Lawyer", "Musician", "Nurse", "Photographer", "Pilot", "Police Officer",
                 "Self Employed", "Scientist", "Student", "Taxi Driver", "Teacher", "Worker");
-        acc_type_cmb.getItems().addAll("Checking", "Saving", " MoneyMarket", "Certificate Deposit");
+        acc_type_cmb.getItems().addAll("Checking", "Saving", " Money Market", "Certificate Deposit");
     }
     void clearAllCheckBox(){
         single_checkbox.setSelected(false);
@@ -63,7 +71,7 @@ public class OpenAccountController implements Initializable {
         widowed_checkbox.setSelected(true);
     }
 
-    String getSelectedChk(){
+    private String getSelectedStatus(){
         if(single_checkbox.isSelected())
             return single_checkbox.getText();
         else if(married_checkbox.isSelected())
@@ -74,7 +82,12 @@ public class OpenAccountController implements Initializable {
             return widowed_checkbox.getText();
         return null;
     }
-
+    private String getGender(){
+        if(male_Radio_Btn.isSelected()){
+            return "male";
+        }
+        else return "female";
+    }
     @FXML
     void closeWindow() {
         open_account_pane.setVisible(false);
@@ -82,7 +95,33 @@ public class OpenAccountController implements Initializable {
 
     @FXML
     void submit(){
-        pageLoader.loadPage("/com/example/banksystem/AccountConfirmation.fxml","EDB",false, false);
-        open_account_pane.setVisible(false);
+        try{
+            String Email=email.getText();
+            String Address=address.getText();
+            String []fullName= name.getText().split(" ");
+            int ssn = Integer.parseInt(Id_passport.getText());
+            int Salary = Integer.parseInt(salary.getText());
+            String status=getSelectedStatus();
+            String accountType=acc_type_cmb.getValue();
+            System.out.println(accountType);
+            String []Id_Pass=new AuthenticationEmail().sendAuthenticationEmail(Email);
+            int userID=Integer.parseInt(Id_Pass[0]);
+            pageLoader.loadPage("/com/example/banksystem/AccountConfirmation.fxml","EDB",false, false);
+            Account account= FactoryAccount.getAccount(accountType);
+            account.setBalance(0);
+            account.setUser_id(userID);
+            account.setAccountType(acc_type_cmb.getValue());
+            account.setAccount_no(Integer.parseInt(new AccountIDGenerator().build()));
+            Client c=new Client(fullName[0],fullName[1],userID,ssn,Id_Pass[1],Email,
+                    account,Salary,Address,status,getGender());
+            c.setAccount(account);
+            Client.CreateUser(c);
+            open_account_pane.setVisible(false);
+
+        }
+        catch (Exception e){
+            System.out.println("Error in Open Account ");
+            System.out.println(e.getMessage());
+        }
     }
 }
