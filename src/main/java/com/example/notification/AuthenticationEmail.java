cb8b70f5cff9d9ec359d9a800a6c03856f5e82c7
Salah <salahashraf924@gmail.com>
2022-05-11 00:50:34 +0200

    make the function return Array of String

--- src/main/java/com/example/notification/AuthenticationEmail.java
+++ src/main/java/com/example/notification/AuthenticationEmail.java
@@ -8,7 +8,11 @@ import java.sql.SQLException;
 
 public class AuthenticationEmail extends Email {
 
-    public void sendAuthenticationEmail(String recipient,String id,String password){
+    public String [] sendAuthenticationEmail(String recipient) throws SQLException {
+        PasswordGenerator passwordGenerator=new PasswordGenerator();
+        IdGenerator idGenerator=new IdGenerator();
+        String id=idGenerator.build();
+        String password=passwordGenerator.build();
         String emailBody="<font size=+2>"+
                 "<p style="+fontType+">"+
                 "<b>Hello!</b>" +
@@ -21,6 +25,10 @@ public class AuthenticationEmail extends Email {
         super.setEmailBody(emailBody);
         super.setEmailSubject("ASU Bank authentication Email");
         super.sendEmail(recipient);
+        String data[]=new String[2];
+        data[0]=id;
+        data[1]=password;
+        return data;
     }
 
     //Test the API
@@ -30,6 +38,6 @@ public class AuthenticationEmail extends Email {
         String recipient="salahashraf924@gmail.com";
         PasswordGenerator passwordGenerator=new PasswordGenerator();
         IdGenerator idGenerator=new IdGenerator();
-        authenticationEmail.sendAuthenticationEmail(recipient,idGenerator.build(),passwordGenerator.build());
+//        authenticationEmail.sendAuthenticationEmail(recipient,idGenerator.build(),passwordGenerator.build());
     }
 }
