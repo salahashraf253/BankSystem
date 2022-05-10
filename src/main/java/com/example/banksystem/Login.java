9c05ebb795f40afd454b6a0fdf910494aceb92b2
Salah <salahashraf924@gmail.com>
2022-05-10 18:59:09 +0200

    modify the sql query

--- src/main/java/com/example/banksystem/Login.java
+++ src/main/java/com/example/banksystem/Login.java
@@ -1,11 +1,8 @@
 package com.example.banksystem;
 
-import com.example.NotificationsObserver.EmailNotification;
-import com.example.UserFactory.FactoryUser;
 import com.example.UserFactory.User;
 import com.example.dataBase.DataBaseMapping;
 import com.example.dataBase.Functions.DataBaseReader;
-import com.example.dataBase.Functions.EncrpytString;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -14,8 +11,7 @@ public class Login {
 
     public static User validateLogin(String userID, String password) throws SQLException {
         DataBaseReader dataBaseReader= new DataBaseReader();
-        String query="select * from user where user_id='" + EncrpytString.getEncrpytedString(userID)
-                + "' and password='" + EncrpytString.getEncrpytedString(password)+"';";
+        String query="select * from user where user_id='" + userID + "' and password='" + password+"';";
         ResultSet resultSet=dataBaseReader.read(query);
         User user= DataBaseMapping.getUser(resultSet);
         dataBaseReader.closeConnection();
