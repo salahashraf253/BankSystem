812a0db9d41ca15195b5cee20e2e6ce49a40009b
Salah <salahashraf924@gmail.com>
2022-05-11 02:39:21 +0200

    implement withdraw function

--- src/main/java/com/example/GUI/HomeController.java
+++ src/main/java/com/example/GUI/HomeController.java
@@ -1,32 +1,42 @@
 package com.example.GUI;
 
 import com.example.UserFactory.Client;
+import com.example.banksystem.Account.Account;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 import java.net.URL;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 
 public class HomeController implements Initializable {
 
     @FXML
     public  Label name_lbl, account_num_lbl ,account_type_lbl,balance_lbl;
+    @FXML
+    public Label rem_balance_lbl;
 
+    private Client client=(Client)LayoutController.user;;
+
+    @FXML
+    private TextField withdraw_amount_txt;
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         try{
             System.out.println("Name: "+LayoutController.user.getFirstname());
 //            name_lbl.setText(LayoutController.user.getFirstname());
             Platform.runLater(() -> {
-                name_lbl.setText(LayoutController.user.getFirstname());
-                Client c=(Client)LayoutController.user;
-                String accountNumber= String.valueOf(c.getAccount().getAccount_no());
+                name_lbl.setText(client.getFirstname());
+                String accountNumber= String.valueOf(client.getAccount().getAccount_no());
                 account_num_lbl.setText(accountNumber);
-                account_type_lbl.setText(c.getAccount().getAccountType());
-                balance_lbl.setText(Integer.toString(c.getSalary()));
+                account_type_lbl.setText(client.getAccount().getAccountType());
+                balance_lbl.setText(Integer.toString(client.getSalary()));
+                rem_balance_lbl.setText(Float.toString(client.getAccount().getBalance()));
+                updateRemainingBalanceLabel();
             });
         }
         catch (Exception e){
@@ -36,7 +46,20 @@ public class HomeController implements Initializable {
     }
 
     @FXML
-    public void withdrawMoney() {
+    public void withdrawMoney() throws SQLException {
+        float amount=Float.parseFloat(withdraw_amount_txt.getText());
+        Account account= client.getAccount();
+        account.setAccount_no(client.getUserId());
+        if(account.canWithdraw(amount)){
+            account.withdraw(amount);
+            account.updateBalance(amount);
+            updateRemainingBalanceLabel();
+        }
+        else System.out.println("Sorry, Your balance is not enough");
+    }
+    private void updateRemainingBalanceLabel(){
+        rem_balance_lbl.setText(Float.toString(client.getAccount().getBalance()));
+
     }
 
 }
