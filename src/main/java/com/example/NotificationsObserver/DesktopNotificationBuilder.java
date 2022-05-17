d3f036d6601aed8fd87e400c4020b43e2bbbc390
Salah <salahashraf924@gmail.com>
2022-05-17 19:01:23 +0200

    create DesktopNotificationBuilder.java

---
+++ src/main/java/com/example/NotificationsObserver/DesktopNotificationBuilder.java
@@ -0,0 +1,18 @@
+package com.example.NotificationsObserver;
+import javafx.util.Duration;
+import tray.animations.AnimationType;
+import tray.notification.NotificationType;
+import tray.notification.TrayNotification;
+
+public class DesktopNotificationBuilder {
+
+    public static void notifyUser(String message, NotificationType notificationType) {
+        TrayNotification tray = new TrayNotification();
+        AnimationType type = AnimationType.POPUP;
+        tray.setAnimationType(type);
+        tray.setTitle("EDB");
+        tray.setMessage(message);
+        tray.setNotificationType(notificationType);
+        tray.showAndDismiss(Duration.seconds(4));
+    }
+}
