be95c74d04c97f5d28c28449e9c482262bd6ecc7
Salah <salahashraf924@gmail.com>
2022-05-20 23:51:04 +0200

    add function update balance

--- src/main/java/com/example/banksystem/Account/SavingsAccount.java
+++ src/main/java/com/example/banksystem/Account/SavingsAccount.java
@@ -1,10 +1,13 @@
-
 package com.example.banksystem.Account;
 
 public class SavingsAccount extends Account{
+    double interestRate;
      public SavingsAccount() {
         accountType = "Savings Account";
+        interestRate=0.1;
     }
-
-
+    public void updateBalanceWithInterestRate(){
+         int yearsPeriod=1;
+         this.setBalance((float) (getBalance()* interestRate*yearsPeriod));
+    }
 }
