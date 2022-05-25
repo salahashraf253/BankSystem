055bb85148410b0d296e574ff41ed1bf9d52702e
Salah <salahashraf924@gmail.com>
2022-05-25 16:30:57 +0200

    edit the constructor

--- src/main/java/com/example/UserFactory/Client.java
+++ src/main/java/com/example/UserFactory/Client.java
@@ -10,8 +10,8 @@ import java.sql.SQLException;
 public class Client extends User {
     private Account account;
     private Transaction transaction;
-    public Client(String firstName, String lastName, int userId, int ssd, String password, String email, Account account,
-                  int salary, String address, String status, String gender) {
+    public Client(String firstName, String lastName, int userId, String ssd, String password, String email, Account account,
+                  int salary, String address, String status, String gender,String occupation) {
         this.account = account;
         this.firstName = firstName;
         this.lastName = lastName;
@@ -23,6 +23,7 @@ public class Client extends User {
         this.Address = address;
         this.status = status;
         this.gender = gender;
+        this.occupation=occupation;
     }
     public Client() {
     }
@@ -32,6 +33,10 @@ public class Client extends User {
     }
 
     public Account getAccount() {
+        if(account==null){
+            System.out.println("Bank account is null");
+        }
+        else System.out.println("bank account not null");
         return this.account;
     }
 
